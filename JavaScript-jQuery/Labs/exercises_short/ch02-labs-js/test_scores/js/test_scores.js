78ae94f8826f5aa3967dc2d5524f757c2c7265f8
unknown <>
2023-03-16 04:48:31 -0500

    LABS_js_ch02

--- JavaScript-jQuery/Labs/exercises_short/ch02-labs-js/test_scores/js/test_scores.js
+++ JavaScript-jQuery/Labs/exercises_short/ch02-labs-js/test_scores/js/test_scores.js
@@ -12,26 +12,27 @@ const score4 = parseInt(prompt("Enter test score"));
 const total = score1 + score2 + score3 + score4;
 
 // calculate the average
-const average = parseInt(total / 4);
+const average = (total / 4);
 //alert(average);
 
 
 // display in alert box
-alert("Score 1 = " + score1 + 
-      "\nScore 2 = " + score2 + 
-      "\nScore 3 = " + score3 + 
-      "\nScore 4 = " + score4 + 
-      "\n\nAverage Score = " + average
-      );
+alert(`Score 1 = ${score1}
+      \nScore 2 = ${score2} 
+      \nScore 3 = ${score3}
+      \nScore 4 = ${score4}
+      \nAverage Score = ${average}
+      `);
        
       
 
       // display in browser page
-const html = `<p>Score 1 = ${score1}</p>
-    <p>Score 2 = ${score2}</p>
-    <p>Score 3 = ${score3}</p>
-    <p>Score 4 = ${score4}</p>
-    <p>Average score = ${average}</p>`;
-    document.write(html);
- 
+const html = 
+      `<p>Score 1 = ${score1}</p>
+      <p>Score 2 = ${score2}</p>
+      <p>Score 3 = ${score3}</p>
+      <p>Score 4 = ${score4}</p>
+      <p>Average score = ${average}</p>`;
+      document.write(html);
+      
 
