45856c598dc6af4d1b8bf1937c04e491d422cffe
loudiamond13 <loudiamond_loyloy@insideranken.org>
2023-04-04 04:54:09 -0500

    JS_HW_ch07

--- JavaScript-jQuery/Homework/exercises_extra/ch07/clock_stopwatch-7.2/js/clock.js
+++ JavaScript-jQuery/Homework/exercises_extra/ch07/clock_stopwatch-7.2/js/clock.js
@@ -3,6 +3,7 @@ const $ = selector => document.querySelector(selector);
 
 const padSingleDigit = num => num.toString().padStart(2, "0");
 
+
 const displayCurrentTime = () => {
     const now = new Date();
     const hours = now.getHours();
@@ -35,41 +36,95 @@ let stopwatchTimer = null;
 let elapsedMinutes = 0;
 let elapsedSeconds = 0;
 let elapsedMilliseconds = 0;
+//const today          = new Date() 
 
-const tickStopwatch = () => {    
+let tick;
+
+const tickStopwatch = () => { 
+    // declaring constants and variables\
+   // let ms      = 00
+    //let secs    = 00
+    //let min     = 00
+
+    
     // increment milliseconds by 10 milliseconds
-    
+    elapsedMilliseconds += 10
+   
     // if milliseconds total 1000, increment seconds by one and reset milliseconds to zero
-    
+    if (elapsedMilliseconds === 1000)
+    {
+        elapsedSeconds++
+        elapsedMilliseconds = 0
+    }
     // if seconds total 60, increment minutes by one and reset seconds to zero
+    if (elapsedSeconds === 60)
+    {
+        elapsedMinutes++
+        elapsedSeconds = 0
+    }
+
+
+
     
     //display new stopwatch time
+    if( elapsedMilliseconds < 10 )
+    {
+    $("#s_ms").textContent = "00" + elapsedMilliseconds
+    }else if(elapsedMilliseconds < 100){
+        $("#s_ms").textContent = "0" + elapsedMilliseconds
+    }else{
+        $("#s_ms").textContent = elapsedMilliseconds
+    }
+   
+    // seconds
+    $("#s_seconds").textContent = padSingleDigit(elapsedSeconds)
     
+    //minutes.
+    $("#s_minutes").textContent = padSingleDigit(elapsedMinutes)
 };
 
 // event handler functions
 const startStopwatch = evt => {
     // prevent default action of link
-        
+    evt.preventDefault()
     // do first tick of stop watch and then set interval timer to tick 
+    //stopwatchTimer = setInterval(tickStopwatch, 10);
+    //
+
+    //console.log(stopwatchTimer)
     // stop watch every 10 milliseconds. Store timer object in stopwatchTimer 
     // variable so next two functions can stop timer.
-    
+    stopwatchTimer = setInterval(tickStopwatch, 10);
+
 };
 
 const stopStopwatch = evt => {
     // prevent default action of link
-        
+    evt.preventDefault()
     // stop timer
+    clearInterval(stopwatchTimer)
     
 };
 
 const resetStopwatch = evt => {
     // prevent default action of link
-        
+        evt.preventDefault()
     // stop timer
-        
+        clearInterval(stopwatchTimer)
     // reset elapsed variables and clear stopwatch display
+   
+    elapsedMinutes = 0;
+    elapsedSeconds = 0;
+    elapsedMilliseconds = 0;
+
+    
+    // seconds
+    $("#s_seconds").textContent = "00"
+    
+    //minutes.
+    $("#s_minutes").textContent = "00"
+
+    $("#s_ms").textContent = "000"
     
 };
 
@@ -79,6 +134,16 @@ document.addEventListener("DOMContentLoaded", () => {
     // won't be needed - clock will just run.
     displayCurrentTime();
     setInterval(displayCurrentTime, 1000);
-	
-	// set up stopwatch event handlers
+
+
+
+	// set up stopwatch event handlers*/
+    //start button
+    $("#start").addEventListener("click", startStopwatch)
+    
+    //stop button
+    $("#stop").addEventListener("click", stopStopwatch)
+
+    //reset
+    $("#reset").addEventListener("click", resetStopwatch)
 });
