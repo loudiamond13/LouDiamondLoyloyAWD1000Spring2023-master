9dc5019ab147f157f13ecfd64aefcf8247e926b8
unknown <>
2023-04-16 11:57:21 -0500

    JS_LAB_ch11-1

--- JavaScript-jQuery/Labs/exercises_short/ch11/faqs-11.1/js/faqs.js
+++ JavaScript-jQuery/Labs/exercises_short/ch11/faqs-11.1/js/faqs.js
@@ -1,6 +1,13 @@
 "use strict";
 $(document).ready( () => {
-    $("#faqs h2").click( evt => {
+    $("#faqs").accordion({
+        collapsible:    true,
+        heightStyle: "content"
+    })
+
+
+});
+    /*$("#faqs h2").click( evt => {
         const h2 = evt.currentTarget;
         $(h2).toggleClass("minus");
         if ($(h2).attr("class") !== "minus") {
@@ -11,5 +18,4 @@ $(document).ready( () => {
         }
         evt.preventDefault();
     });
-    $("#faqs").find("a:first").focus();
-});
+    $("#faqs").find("a:first").focus();*/
